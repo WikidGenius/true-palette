112daed3fefec67881a1d78d45e4a9ce87515629
Jeff Gomez <jeffreyrgomez@gmail.com>
2026-06-27 15:46:18 -0600

    Add patch chunk thirteen d

---
+++ .tp/z13d.py
@@ -0,0 +1,1 @@
+add('Hair Weight <span>visual depth</span>','<b>How to test:</b> look at roots or the most natural section. Does the hair add soft lightness, middle structure, or strong dark weight?')
