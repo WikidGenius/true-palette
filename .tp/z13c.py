48a235fcc63b1a59e62aa3f8c6d1f1fb5ee43550
Jeff Gomez <jeffreyrgomez@gmail.com>
2026-06-27 15:46:10 -0600

    Add patch chunk thirteen c

---
+++ .tp/z13c.py
@@ -0,0 +1,1 @@
+add('Complexion Value <span>light to deep</span>','<b>How to test:</b> step back from the mirror. Ignore demographic labels and judge the total light-to-deep value compared with hair and eyes.')
