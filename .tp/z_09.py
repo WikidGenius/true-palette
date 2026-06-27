f23b9af9c68302cadcfe95711f6c1fba1fd478b8
Jeff Gomez <jeffreyrgomez@gmail.com>
2026-06-27 15:43:42 -0600

    Add patch chunk nine

---
+++ .tp/z_09.py
@@ -0,0 +1,4 @@
+kit+='''        <li><b>Gold and silver metal</b><span>Use jewelry, watch bands, buttons, or hardware.</span></li>
+        <li><b>Indirect daylight and mirror</b><span>Face a window and avoid harsh lighting.</span></li>
+      </ul>
+'''
