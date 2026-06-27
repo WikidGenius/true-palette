70f93cbd5af044e1b3b31a33c77008dfa6b99f03
Jeff Gomez <jeffreyrgomez@gmail.com>
2026-06-27 15:46:39 -0600

    Add patch chunk thirteen f

---
+++ .tp/z13f.py
@@ -0,0 +1,1 @@
+add('Eye Value <span>light to deep</span>','<b>How to test:</b> use daylight, then squint slightly. Do the eyes read bright and reflective, moderate and readable, or deep and inky?')
