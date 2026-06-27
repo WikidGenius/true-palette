a564e51d3a3083440131ab7590792550e1908073
Jeff Gomez <jeffreyrgomez@gmail.com>
2026-06-27 15:43:59 -0600

    Add patch chunk ten a

---
+++ .tp/z10a.py
@@ -0,0 +1,3 @@
+kit+='''      <h3>Optional confirmers</h3>
+      <div class="prop-tags"><span class="prop-tag">black or charcoal fabric</span><span class="prop-tag">soft gray fabric</span></div>
+'''
