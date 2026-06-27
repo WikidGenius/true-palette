609872314c34222bc63942293ae1a1e5b40eb06a
Jeff Gomez <jeffreyrgomez@gmail.com>
2026-06-27 15:44:47 -0600

    Add patch chunk ten e

---
+++ .tp/z10e.py
@@ -0,0 +1,2 @@
+kit+='''      <p class="note"><b>Best setup:</b> test one prop at a time. Look at the face first. The best prop should make the person look clearer, healthier, and more awake.</p>
+    </aside>'''
