7014d61c52f76a7db340d01cbff9912eb52ec881
Jeff Gomez <jeffreyrgomez@gmail.com>
2026-06-27 15:48:26 -0600

    Add patch chunk fifteen c

---
+++ .tp/z_15c.py
@@ -0,0 +1,1 @@
+s=s.replace('Neither fabric clearly wins. Finish and outfit context matter more.','Choose this if both white and cream look wearable and neither one clearly drains or brightens you.')
